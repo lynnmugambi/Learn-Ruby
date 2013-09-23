439830e29dfffc4e27a347a29fb5c96825414c02
lynnmugambi <mugambilynn@gmail.com>
2013-09-23 12:14:02 +0300

    exercise 2

---
+++ ex2.rb
@@ -0,0 +1,10 @@
+# A comment, thi is do you can read your program later.
+# anything after the # will be ignored
+
+puts "i could have code like this."
+
+# you can also use a comment to " disable" or comment out a piece of code.
+# puts "This wont run"
+
+puts "This will run!"
+
